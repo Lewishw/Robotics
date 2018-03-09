585fe79e99c525a996c29643cf29db8c1ed2457c
Louis <louis.hawley@gmail.com>
2018-03-09 07:50:10 -0500

    Automatic Backup @ 2018. 03. 09. (금) 07:50:10 EST

--- Main_Script/Chap2/Orientation3d.m
+++ Main_Script/Chap2/Orientation3d.m
@@ -11,3 +11,13 @@ T = rotx(pi/2)*roty(pi/2)*rotz(pi/2);
 tranimate(T,'3d')
 
 tripleangle
+pause()
+
+%%
+close all
+X = transl(3,4,-4);
+angles = 0:0.3:15;
+tw = Twist('R',[1 1 1],[3 4 2],0.2);
+
+tranimate(@(theta) tw.T(theta)*X, angles, ...
+    'length',0.5, 'retain','rgb','notext')
